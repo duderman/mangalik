a1406e992852681b6d2bc674d7c7ea24e598b4c5
Nikolay Norkin <nikolay.norkin@forwardpmx.com>
2020-06-13 16:58:18 +0100

    email&compose

---
+++ email_sender/index.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const port = 8080;
+const host = '0.0.0.0';
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+  let transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
+  let name = req.body.name
+  let email = req.body.email
+  let msg = req.body.msg
+  let body = "Заказик от " + name + " <" + email + ">: " + msg
+
+  transporter.sendMail({
+    from: '"Заказик" <заказик@мангалик.рф>',
+    to: process.env.EMAILS_RECEIVER,
+    subject: "Ого! Новый заказик!",
+    text: body,
+    html: "<b>"+body+"</b>",
+  }).then(function() {
+    res.status(201).send('Принял');
+  }).catch(function(){
+    res.status(500).send('Не смогла :(');
+  })
+});
+
+console.log(`Listening on port ${port}`);
+app.listen(port, host);
